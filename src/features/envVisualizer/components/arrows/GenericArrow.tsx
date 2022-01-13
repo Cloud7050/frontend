e915becd841a7762851f5ea244d9055308d68ace
Jordan Soh <jordansoh@Jordans-MacBook-Pro.local>
2022-01-13 21:55:19 +0800

    First commit

--- src/features/envVisualizer/components/arrows/GenericArrow.tsx
+++ src/features/envVisualizer/components/arrows/GenericArrow.tsx
@@ -68,7 +68,7 @@ export class GenericArrow implements Visible, Hoverable {
       (points, step) => [...points, ...step(points[points.length - 2], points[points.length - 1])],
       [this.from.x, this.from.y]
     );
-    points.splice(0, 2);
+    points.splice(0, 2); 
 
     return (
       <KonvaArrow
@@ -81,6 +81,48 @@ export class GenericArrow implements Visible, Hoverable {
         key={Layout.key++}
         onMouseEnter={this.onMouseEnter}
         onMouseLeave={this.onMouseLeave}
+        sceneFunc={function(ctx,shape){
+          ctx.beginPath();
+          ctx.moveTo(points[0],points[1]);
+          if (points.length === 4) {
+              ctx.lineTo(points[2],points[3]);
+          } else {
+              const borderRadius = 40;
+              let n = 0;
+                 
+              while (n < points.length - 4) {
+                  const dx1 = (points[n + 2] - points[n + 0]);
+                  const dy1 = (points[n + 3] - points[n + 1]);
+                  const br1 = Math.min(borderRadius, Math.max(Math.abs(dx1 / 2),Math.abs(dy1 / 2)));
+  
+                  const dx2 = (points[n + 2 + 2] - points[n + 0 + 2]);
+                  const dy2 = (points[n + 3 + 2] - points[n + 1 + 2]);
+                  const br2 = Math.min(borderRadius, Math.max(Math.abs(dx2 / 2), Math.abs(dy2 / 2)));
+  
+                  const br = Math.min(br1, br2);
+  
+                  const x1 = points[n + 0] + (Math.abs(dx1) - br) * Math.sign(dx1);
+                  const y1 = points[n + 1] + (Math.abs(dy1) - br) * Math.sign(dy1);
+  
+                  ctx.lineTo(x1, y1);
+                  n+=2;
+                  const x2 = points[n + 0] + br * Math.sign(dx2);
+                  const y2 = points[n + 1] + br * Math.sign(dy2);
+  
+                  ctx.quadraticCurveTo(points[n + 0], points[n + 1], x2, y2);
+              }
+              ctx.lineTo(points[points.length - 2],points[points.length - 1]);     
+          }
+          const arrowLen = 10;   // length of arrow in pixels
+          const angle = Math.atan2(points[points.length - 1] - points[points.length - 3], 
+                                   points[points.length - 2] - points[points.length - 4]);
+          ctx.lineTo(points[points.length - 2] - arrowLen * Math.cos(angle - Math.PI / 6), 
+                     points[points.length - 1] - arrowLen * Math.sin(angle - Math.PI / 6));
+          ctx.moveTo(points[points.length - 2], points[points.length - 1]);
+          ctx.lineTo(points[points.length - 2] - arrowLen * Math.cos(angle + Math.PI / 6), 
+                     points[points.length - 1] - arrowLen * Math.sin(angle + Math.PI / 6));
+          ctx.strokeShape(shape);
+      }}
       />
     );
   }
